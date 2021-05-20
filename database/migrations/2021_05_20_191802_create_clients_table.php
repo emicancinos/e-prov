d4a5d7be9118ad96d84318a3435bb68f18cc393a
Emiliano Cancinos <ecancinos@octopus.com.ar>
2021-05-20 16:23:30 -0300

    Creando modelos

---
+++ database/migrations/2021_05_20_191802_create_clients_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('last_name');
+            $table->timestamps();
+            $table->unsignedInteger('user_id');
+            // $table->unsignedInteger('city_id');
+            
+            
+            // $table->foreign('city_id')->references('id')->on('cities');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
---
+++ database/migrations/2021_05_20_191824_create_providers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProvidersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('providers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('cuit_number');
+            $table->integer('enrollment_number');
+            $table->string('business_name');
+            $table->timestamps();
+            $table->unsignedInteger('user_id');
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('providers');
+    }
+}
