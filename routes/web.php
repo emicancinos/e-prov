02d6d516301ba44ad38529488a79fa1b3c242cf7
denisearz <darzac@octopus.com.ar>
2021-05-21 11:45:15 -0300

    feature/login se crea registro

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/user', 'UserController@index');
+Route::post('user', 'UserController@insert');
+Route::delete('user/{id}', 'UserController@delete');
