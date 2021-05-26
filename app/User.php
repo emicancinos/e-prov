9448f67cbc3d1bcbd247b9b0810590a09205b2f4
Emiliano Cancinos <ecancinos@octopus.com.ar>
2021-05-26 14:38:47 -0300

    seeders

--- app/User.php
+++ app/User.php
@@ -35,10 +35,7 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-
+  
     public function getJWTIdentifier()
     {
         return $this->getKey();
