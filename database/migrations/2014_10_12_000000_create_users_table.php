c387062afe6b752876b192fced0569bcce891c21
Emiliano Cancinos <ecancinos@octopus.com.ar>
2021-05-27 11:58:49 -0300

    user_type

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table-> enum('user_type', [
+            $table->enum('user_type', [
                 'PROVEEDOR',
                 'CLIENTE'
                 ]);
