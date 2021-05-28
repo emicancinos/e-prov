18cca11a8a2ae3b5519ffb0ee0b458b5812f58b4
Emiliano Cancinos <ecancinos@octopus.com.ar>
2021-05-28 09:11:06 -0300

    Se agrega user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,61 +1,85 @@
 <?php   
-// namespace App\Http\Controllers;
 
-//     use App\User;
-//     use Illuminate\Http\Request;
-//     use Illuminate\Support\Facades\Hash;
-//     use Illuminate\Support\Facades\Validator;
-//     use JWTAuth;
-//     use Tymon\JWTAuth\Exceptions\JWTException;
-//     use Tymon\JWTAuth\Facades\JWTAuth as FacadesJWTAuth;
+namespace App\Http\Controllers;
 
-// class UserController extends Controller
-// {
-//     public function authenticate(Request $request)
-//     {
-//         $credentials = $request->only('email', 'password');
-//         try {
-//             if (! $token = FacadesJWTAuth::attempt($credentials)) {
-//                 return response()->json(['error' => 'invalid_credentials'], 400);
-//             }
-//         } catch (JWTException $e) {
-//             return response()->json(['error' => 'could_not_create_token'], 500);
-//         }
-//         return response()->json(compact('token'));
-//     }
-//     public function getAuthenticatedUser()
-//     {
-//             if (!$user = FacadesJWTAuth::parseToken()->authenticate()) {
-//                     return response()->json(['user_not_found'], 404);
-//             }
-            
-//             return response()->json(compact('user'));
-//     }
+use App\User;
+use Illuminate\Http\Request;
 
-//     /**
-//      * REGISRO
-//      */
-    
-//     public function register(Request $request)
-//         {
-//                 $validator = Validator::make($request->all(), [
-//                 'name' => 'required|string|max:255',
-//                 'email' => 'required|string|email|max:255|unique:users',
-//                 'password' => 'required|string|min:6|confirmed',
-//             ]);
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
 
-//             if($validator->fails()){
-//                     return response()->json($validator->errors()->toJson(), 400);
-//             }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
 
-//             $user = User::create([
-//                 'name' => $request->get('name'),
-//                 'email' => $request->get('email'),
-//                 'password' => Hash::make($request->get('password')),
-//             ]);
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
 
-//             $token = FacadesJWTAuth::fromUser($user);
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        //
+    }
 
-//             return response()->json(compact('user','token'),201);
-//         }
-// }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
