77e12baa55b0537b1b537d7702cc81a5c1d16d5e
Jesica <jfirus@octopus.com.ar>
2021-06-08 14:58:45 -0300

    modificación de los seeds para la inserción correcta en la tabla clientes y proveedores

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -8,6 +8,8 @@ use App\provider;
 use App\User;
 use App\client;
 use App\ProvidersCategories;
+use App\speciality;
+use App\SpecialityCategories;
 
 
 
@@ -26,10 +28,28 @@ class DatabaseSeeder extends Seeder
             SpecialitySeeder::class,
             ]);
             factory(User::class, 200)->create();
+
+            $clientes = User::where('user_type', 'CLIENTE')
+                        -> get();
+            foreach($clientes as $cliente){
+                factory(client::class)->create(['user_id' => $cliente->id]);
+            }
+
+            $providers = User::where('user_type', 'PROVEEDOR')
+                        -> get();
+            foreach($providers as $provider){
+                factory(provider::class)->create(['user_id' => $provider->id]);
+            }
+
+            factory(ProvidersCategories::class, 150)->create();
+
+
+            //factory(SpecialityCategories::class, 150)->create();
+            /*
             factory(client::class, 50)->create();
             factory(provider::class, 100)->create();
             factory(ProvidersCategories::class, 150)->create();
-
+            */
         
     }
 }
